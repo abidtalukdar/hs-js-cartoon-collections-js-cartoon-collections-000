a753f5db61e3a06cd167fd0d212475d4b6955cde
Abid Talukdar <abiditalukdar@gmail.com>
2019-12-23 23:03:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 function dwarfRollCall(dwarves) {
+  var dwarf = "";
+  var i; 
+  for (i=0; i<=dwarves.length - 1; i++) {
+    dwarf+=(i+1 + ". " + dwarves[i] + " ");
+  }
+  return dwarf;
+}
+  
+
+function summonCaptainPlanet(planeteerCalls) {
+var list = [];
+  var i; 
+  for (i=0; i<=planeteerCalls.length - 1; i++) {
+    list.push( (planeteerCalls[i]).toUpperCase() + "!");
+  }
+  return list ;
 }
 
-function summonCaptainPlanet(planeteerCalls){
-}
 
 function longPlaneteerCalls(words) {
+  for (var i=0; i<=words.length - 1; i++) {
+    if (words[i].length>4) {
+      return true;
+    } 
+  }
+  return false;
 }
 
+
 function findTheCheese (foods) {
+  for (var i=0; i<=foods.length - 1; i++) {
+    if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") {
+      return foods[i];
+    } 
+  }
+  return "no cheese!";
 }
+
+
